dc810f77e899d55bbaa929e1ccba0b2a45b763e3
hassan95eb <19h.amini95@gmail.com>
2023-02-22 22:07:46 +0330

    add new changes on app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 const Cornometer=document.querySelector(".timer");
 var Timer=[0,0,0,0];
+var runner=false;
+function Standardtimer(time){
+    if(time<=9){
+        time="0"+time;
+    }
+    return time;
+}
 function runtimer(){
-    let currentTime= Timer[0] +":" + Timer[1]+":"+Timer[2];
+    let currentTime= Standardtimer(Timer[0]) +":" + Standardtimer(Timer[1])+":"+Standardtimer(Timer[2]);
     Timer[3]++;
     Timer[0]=Math.floor((Timer[3]/100)/60);
     Timer[1]=Math.floor((Timer[3]/100)-(Timer[0]*60));
     Timer[2]=Math.floor(Timer[3]-(Timer[1]*100)-(Timer[0]*6000));
     Cornometer.innerHTML=currentTime;
 }
-setInterval(runtimer,10);
+
+function textTimer(){
+    let textAreaLength=document.querySelector("#text-area").value.length;
+    if (textAreaLength==0 && !runner){
+        runner=true;
+        setInterval(runtimer,10);
+    }
+}
+var textArea=document.querySelector("#text-area");
+textArea.addEventListener("keypress",textTimer);
